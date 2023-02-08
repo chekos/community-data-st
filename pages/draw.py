2f3c2ce2f88057652f33c5d183ccbe17f491b154
chekos <sergio@cimarron.io>
2023-02-08 11:42:25 -0800

    cleaer

--- pages/draw.py
+++ pages/draw.py
@@ -9,13 +9,12 @@ from streamlit_folium import st_folium
 m = folium.Map(location=[37.8181063, -122.2851045], zoom_start=15)
 Draw(export=True).add_to(m)
 
-c1, c2 = st.columns(2)
-with c1:
-    output = st_folium(m, width=700, height=500)
+output = st_folium(m, width=700, height=500)
 
-with c2:
-    st.write(output)
-    coords = output["last_clicked"]
-    if coords:
-      g = geocoder.google([coords["lat"], coords["lng"]], method = "reverse")
-      st.write(g)
+st.write(output)
+coords = output["last_clicked"]
+if coords:
+    lat = coords.get("lat", 37.818)
+    lng = coords.get("lng", -122.285)
+    g = geocoder.google([lat, lng], method = "reverse")
+    st.write(g.json)
